110e6e96e511cc8cad4cea4f689ce6fa2b64afec
tgwgraham <tgwgraham@gmail.com>
2024-07-13 16:20:41 -0700

    Added convenient fraction chromatin bound (D < 0.1) output.

--- run_papa_saspt.py
+++ run_papa_saspt.py
@@ -26,6 +26,23 @@ au.plot_Nlocs_bycycle_colors(config,locs_df,byexperiment=True,closefig=True)
 # Do state array analysis of each condition
 [SA,posterior_occs,condition_names] = au.analyze_PAPA_DR_stateArray(config,nworkers=nworkers,closefig=True)
 
+# Print to the command line and write out a text file with fractions bound (D < 0.1) for all conditions
+D = SA.likelihood.diff_coefs
+with open('figures/fbound.txt','w') as fh:
+    print('Fraction bound (D < 0.1)')
+    for j,c in enumerate(condition_names):
+        outstring = f'{c}\t{posterior_occs[j,D <=.1].sum():.4f}'
+        print(outstring)
+        fh.write(outstring + '\n')
+
 # Do state array analysis of each condition, subsampling the same number of trajectories for each
 [SA_sameN,posterior_occs_sameN,condition_names_sameN] = au.analyze_PAPA_DR_stateArray_sameN(config,nworkers=nworkers,randseed=randseed,ignoreOther=True,closefig=True)
 
+# Print to the command line and write out a text file with fractions bound (D < 0.1) for all conditions
+D = SA_sameN.likelihood.diff_coefs
+with open('figures_sameN/fbound.txt','w') as fh:
+    print('Fraction bound (D < 0.1)')
+    for j,c in enumerate(condition_names_sameN):
+        outstring = f'{c}\t{posterior_occs_sameN[j,D <=.1].sum():.4f}'
+        print(outstring)
+        fh.write(outstring + '\n')
